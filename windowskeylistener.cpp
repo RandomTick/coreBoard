6d848a5ed983d7b5bfa5a0bc6ad23c44be4909a3
christofkern <christof.m.kern@gmail.com>
2024-01-22 14:13:31 +0100

    feat: add ability to differentiate between regular and numbad enter key

--- windowskeylistener.cpp
+++ windowskeylistener.cpp
@@ -9,14 +9,36 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0 && instance) {
         KBDLLHOOKSTRUCT *pkbhs = (KBDLLHOOKSTRUCT *)lParam;
         if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
-            std::cout << "Key Pressed: " << pkbhs->vkCode << std::endl;
-            emit instance->keyPressed(pkbhs->vkCode);
+            if(pkbhs->vkCode == VK_RETURN) {
+                if ((pkbhs->flags & LLKHF_EXTENDED) == LLKHF_EXTENDED) {
+                    //std::cout << "Numpad Enter Pressed" << std::endl;
+                    emit instance->keyPressed(VK_SEPARATOR);
+                } else {
+                    //std::cout << "Regular Enter Pressed" << std::endl;
+                    emit instance->keyPressed(VK_RETURN);
+                }
+            } else {
+                std::cout << "Key Pressed: " << pkbhs->vkCode << std::endl;
+                emit instance->keyPressed(pkbhs->vkCode);
+            }
         } else if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
-            emit instance->keyReleased(pkbhs->vkCode);
+            if(pkbhs->vkCode == VK_RETURN) {
+                if ((pkbhs->flags & LLKHF_EXTENDED) == LLKHF_EXTENDED) {
+                    //std::cout << "Numpad Enter Released" << std::endl;
+                    emit instance->keyReleased(VK_SEPARATOR);
+                } else {
+                    //std::cout << "Regular Enter Released" << std::endl;
+                    emit instance->keyReleased(VK_RETURN);
+                }
+            } else {
+                //std::cout << "Key Released: " << pkbhs->vkCode << std::endl;
+                emit instance->keyReleased(pkbhs->vkCode);
+            }
         }
     }
     return CallNextHookEx(hHook, nCode, wParam, lParam);
 }
+
 
 WindowsKeyListener::WindowsKeyListener(QObject *parent) : QObject(parent) {
     // Constructor
