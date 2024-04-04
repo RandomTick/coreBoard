619c75d7b36f0547f9dfc34e43344b77389509a7
christofkern <christof.m.kern@gmail.com>
2024-04-04 12:46:24 +0200

    chore: refactor resizableRectItem from header only to header + cpp

--- resizablerectitem.cpp
+++ resizablerectitem.cpp
@@ -1,2 +1,29 @@
-#include "resizablerectitem.h"
+#include "ResizableRectItem.h"
 
+ResizableRectItem::ResizableRectItem(const QRectF &rect, const QString &text, QGraphicsItem *parent)
+    : QGraphicsRectItem(rect, parent) {
+    textItem = new QGraphicsTextItem(this);
+    textItem->setPlainText(text);
+    centerText();
+}
+
+void ResizableRectItem::setText(const QString &text) {
+    textItem->setPlainText(text);
+    centerText();
+}
+
+void ResizableRectItem::setRect(const QRectF &rect) {
+    QGraphicsRectItem::setRect(rect);
+    centerText(); // Recenter the text whenever the rectangle size changes
+}
+
+void ResizableRectItem::setRect(qreal x, qreal y, qreal w, qreal h) {
+    setRect(QRectF(x, y, w, h));
+}
+
+void ResizableRectItem::centerText() {
+    // Ensure the text is centered within the new rectangle bounds
+    QRectF textRect = textItem->boundingRect();
+    QPointF center = rect().center() - QPointF(textRect.width() / 2, textRect.height() / 2);
+    textItem->setPos(center);
+}
--- resizablerectitem.h
+++ resizablerectitem.h
@@ -6,36 +6,15 @@
 
 class ResizableRectItem : public QGraphicsRectItem {
 public:
-    ResizableRectItem(const QRectF &rect, QString text, QGraphicsItem *parent = nullptr)
-        : QGraphicsRectItem(rect, parent) {
-        textItem = new QGraphicsTextItem(this);
-        textItem->setPlainText(text); // Set default text or pass it in the constructor
-        centerText();
-    }
+    ResizableRectItem(const QRectF &rect, const QString &text, QGraphicsItem *parent = nullptr);
 
-    void setText(const QString &text) {
-        textItem->setPlainText(text);
-        centerText();
-    }
-
-    void setRect(const QRectF &rect) {
-        QGraphicsRectItem::setRect(rect);
-        centerText(); // Recenter the text whenever the rectangle size changes
-    }
-
-    void setRect(qreal x, qreal y, qreal w, qreal h) {
-        setRect(QRectF(x, y, w, h));
-    }
+    void setText(const QString &text);
+    void setRect(const QRectF &rect);
+    void setRect(qreal x, qreal y, qreal w, qreal h);
 
 private:
     QGraphicsTextItem *textItem;
-
-    void centerText() {
-        // Ensure the text is centered within the new rectangle bounds
-        QRectF textRect = textItem->boundingRect();
-        QPointF center = rect().center() - QPointF(textRect.width() / 2, textRect.height() / 2);
-        textItem->setPos(center);
-    }
+    void centerText();
 };
 
 #endif // RESIZABLERECTITEM_H
