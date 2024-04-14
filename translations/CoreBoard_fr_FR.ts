b1ad3c880e9a7f15075e59c2a575423eff95859d
Christof Kern <79835127+christofkern@users.noreply.github.com>
2024-04-14 13:50:48 +0200

    fix: update gitignore to include tm files

--- translations/CoreBoard_fr_FR.ts
+++ translations/CoreBoard_fr_FR.ts
@@ -4,43 +4,74 @@
 <context>
     <name>LayoutEditor</name>
     <message>
-        <location filename="../layouteditor.cpp" line="47"/>
-        <location filename="../layouteditor.cpp" line="81"/>
+        <location filename="../layouteditor.cpp" line="38"/>
+        <location filename="../layouteditor.cpp" line="153"/>
+        <source>Open Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../layouteditor.cpp" line="55"/>
+        <location filename="../layouteditor.cpp" line="152"/>
         <source>Add Shape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../layouteditor.cpp" line="90"/>
+        <source>Select Layout File to Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../layouteditor.cpp" line="90"/>
+        <source>Layout Files (*.json);;All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
         <location filename="../version_info.ui" line="14"/>
+        <location filename="../build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="74"/>
+        <location filename="../build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="74"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>CoreBoard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../mainwindow.ui" line="28"/>
+        <location filename="../build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="78"/>
+        <location filename="../build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="78"/>
         <source>Window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="36"/>
+        <location filename="../mainwindow.ui" line="35"/>
         <location filename="../version_info.ui" line="65"/>
+        <location filename="../build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="79"/>
+        <location filename="../build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="79"/>
         <source>Info</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="45"/>
+        <location filename="../mainwindow.ui" line="44"/>
+        <location filename="../build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="75"/>
+        <location filename="../build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="75"/>
         <source>Visualization</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="50"/>
+        <location filename="../mainwindow.ui" line="49"/>
+        <location filename="../build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="76"/>
+        <location filename="../build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="76"/>
         <source>Edit Layout</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="55"/>
+        <location filename="../build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="77"/>
+        <location filename="../build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/CoreBoard_autogen/include/ui_mainwindow.h" line="77"/>
         <source>Open Layout</source>
         <translation type="unfinished"></translation>
     </message>
@@ -63,6 +94,19 @@
     <message>
         <location filename="../version_info.ui" line="79"/>
         <source>Binding</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SymbolInputDialog</name>
+    <message>
+        <location filename="../symbolinputdialog.cpp" line="40"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../symbolinputdialog.cpp" line="45"/>
+        <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
